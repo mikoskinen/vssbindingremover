309a39ad34466b18d4c132eb3680277e2f885880
Yuri Danilchenko <yuri@danilchenko.com>
2012-12-18 19:03:57 -0200

    Updated solution to new Visual Studio & .NET verions, added support for more project types and added more detailed cleanup.
    - Updated solution to Visual Studio 2010.
    - Updated project to .NET 4.0.
    - Updated regular expressions to remove blank lines in solution and
    project files after the cleanup.
    - Added code for removing .suo files.
    - Modified code to remove read-only property on all files.
    - Added support for DTS (.dtproj) and Deployment (.vdproj) project
    types.

--- VSSBindingRemover/VSSRemover.cs
+++ VSSBindingRemover/VSSRemover.cs
@@ -37,11 +37,13 @@ namespace VSSBindingRemover
             vssFiles.AddRange(selectedPath.GetFiles("*.vssscc", SearchOption.AllDirectories));
             vssFiles.AddRange(selectedPath.GetFiles("*.scc", SearchOption.AllDirectories));
             vssFiles.AddRange(selectedPath.GetFiles("*.vspscc", SearchOption.AllDirectories));
+            vssFiles.AddRange(selectedPath.GetFiles("*.suo", SearchOption.AllDirectories));
         }
 
         private void RemoveReadOnlyAttributes()
         {
-            var allFiles = slnFiles.Union(projFiles).Union(vssFiles).ToList();
+            // Remove Read-Only attributes from all files
+            var allFiles = new List<FileInfo>(selectedPath.GetFiles("*", SearchOption.AllDirectories));
 
             allFiles.ForEach(f => f.IsReadOnly = false);
         }
@@ -54,25 +56,25 @@ namespace VSSBindingRemover
         private void EditSolutionFiles()
         {
             slnFiles.ForEach(sln =>
-                                 {
-                                     string fullName = sln.FullName;
-                                     string relPath = sln.Directory.FullName.Replace(selectedPath.FullName, string.Empty);
+            {
+                string fullName = sln.FullName;
+                string relPath = sln.Directory.FullName.Replace(selectedPath.FullName, string.Empty);
 
-                                     StreamReader reader = sln.OpenText();
-                                     String text = reader.ReadToEnd();
-                                     reader.Close();
-                                     string regex = "\tGlobalSection\\(SourceCodeControl\\) [\\s\\S]*? EndGlobalSection\r\n";
-                                     RegexOptions options = ((RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline) | RegexOptions.IgnoreCase);
-                                     Regex reg = new Regex(regex, options);
+                StreamReader reader = sln.OpenText();
+                String text = reader.ReadToEnd();
+                reader.Close();
+                string regex = @"\tGlobalSection\(SourceCodeControl\) [\s\S]*? EndGlobalSection\r\n";
+                RegexOptions options = ((RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline) | RegexOptions.IgnoreCase);
+                Regex reg = new Regex(regex, options);
 
-                                     text = reg.Replace(text, string.Empty);
-                                     if (text.StartsWith(Environment.NewLine))
-                                         text = text.Remove(0, 2);
-                                     StreamWriter writer = new StreamWriter(fullName);
-                                     writer.Write(text);
-                                     writer.Flush();
-                                     writer.Close();
-                                 });
+                text = reg.Replace(text, string.Empty);
+                if (text.StartsWith(Environment.NewLine))
+                    text = text.Remove(0, 2);
+                StreamWriter writer = new StreamWriter(fullName);
+                writer.Write(text);
+                writer.Flush();
+                writer.Close();
+            });
         }
 
         private void EditProjectFiles()
@@ -86,11 +88,25 @@ namespace VSSBindingRemover
                 String text = reader.ReadToEnd();
                 reader.Close();
 
-                string regex = "\"*<*Scc.*(>|\\W=\\W\").*?(>|%\")";
+                string regex = "";
+                string replacementString = "";
+
+                if (proj.Extension.Equals(".dtproj"))
+                {
+                    regex = @"^.*(State).*\n?";
+                    replacementString = "<State>$base64$PFNvdXJjZUNvbnRyb2xJbmZvIHhtbG5zOnhzZD0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEiIHhtbG5zOnhzaT0iaHR0cDovL3d3dy53My5vcmcvMjAwMS9YTUxTY2hlbWEtaW5zdGFuY2UiIHhtbG5zOmRkbDI9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYW5hbHlzaXNzZXJ2aWNlcy8yMDAzL2VuZ2luZS8yIiB4bWxuczpkZGwyXzI9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYW5hbHlzaXNzZXJ2aWNlcy8yMDAzL2VuZ2luZS8yLzIiIHhtbG5zOmRkbDEwMF8xMDA9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYW5hbHlzaXNzZXJ2aWNlcy8yMDA4L2VuZ2luZS8xMDAvMTAwIiB4bWxuczpkZGwyMDA9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYW5hbHlzaXNzZXJ2aWNlcy8yMDEwL2VuZ2luZS8yMDAiIHhtbG5zOmRkbDIwMF8yMDA9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vYW5hbHlzaXNzZXJ2aWNlcy8yMDEwL2VuZ2luZS8yMDAvMjAwIiB4bWxuczpkd2Q9Imh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vRGF0YVdhcmVob3VzZS9EZXNpZ25lci8xLjAiPg0KICA8RW5hYmxlZD5mYWxzZTwvRW5hYmxlZD4NCiAgPFByb2plY3ROYW1lPjwvUHJvamVjdE5hbWU+DQogIDxBdXhQYXRoPjwvQXV4UGF0aD4NCiAgPExvY2FsUGF0aD48L0xvY2FsUGF0aD4NCiAgPFByb3ZpZGVyPjwvUHJvdmlkZXI+DQo8L1NvdXJjZUNvbnRyb2xJbmZvPg==</State>";
+                }
+                else
+                {
+                    regex = @"^.*(SccProjectName|SccLocalPath|SccAuxPath|SccProvider).*\n?";
+                    replacementString = string.Empty;
+                }
+
                 RegexOptions options = ((RegexOptions.IgnorePatternWhitespace | RegexOptions.Multiline) | RegexOptions.IgnoreCase);
                 Regex reg = new Regex(regex, options);
 
-                text = reg.Replace(text, string.Empty);
+                text = reg.Replace(text, replacementString);
+
                 StreamWriter writer = new StreamWriter(fullName);
                 writer.Write(text);
                 writer.Flush();
